3db5ee9bc4705d3c90771323fb59d226e73ae5a2
eduardroid <eduardo.delcastillo.huaccha@gmail.com>
2018-07-24 00:56:08 -0500

    first commit

---
+++ WebCrawlerTests/CrawlerEngineTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebCrawler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Tests
+{
+    [TestClass()]
+    public class CrawlerEngineTests
+    {
+        [TestMethod()]
+        public void GoogleSearchTest()
+        {
+            ICrawler crawler = new CrawlerEngine();
+            SearchResult result=crawler.Search(new GoogleSearchEngine(), "Java script");
+
+
+            Assert.IsTrue(result.Value>0);
+        }
+        [TestMethod()]
+        public void MSNSearchTest()
+        {
+            ICrawler crawler = new CrawlerEngine();
+            SearchResult result = crawler.Search(new MsnSearchEngine(), "Java script");
+
+
+            Assert.IsTrue(result.Value > 0);
+        }
+    }
+}
